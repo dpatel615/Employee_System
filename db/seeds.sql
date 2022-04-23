68dc1ef437a72b413740604dcc303d6cc555645b
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-04-23 15:14:16 -0400

    Initial code

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (name)
+VALUES 
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 80000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Didhiti', 'Patel', 2, null),
+('Drusti', 'Patel', 1, 1),
+('Mary', 'Brown', 4, null),
+('Nick', 'Jones', 3, 3),
+('Tyler', 'Moore', 6, null),
+('Ana', 'Ross', 5, 5),
+('Alex', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
+
